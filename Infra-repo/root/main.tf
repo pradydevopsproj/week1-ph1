7e4ae698d270517cd87353af73d40f9e6985cc81
pradydevopsproj <prady26devops@gmail.com>
2026-08-17 09:02:06 +0000

    checkovs commented for temeporary

--- Infra-repo/root/main.tf
+++ Infra-repo/root/main.tf
@@ -6,10 +6,10 @@ module "vpc" {
   tags             = var.tags
 }
 
-// commneted as "Checkovs" complaining since it not being used by any resource
-# module "SG" {
-#   source     = "../modules/SG"
-#   depends_on = [module.vpc] # this depends_on not required since vpc_id = module.vpc.vpc_id already creates an implicit dependency.
-#   vpc_id     = module.vpc.vpc_id
-#   tags       = var.tags
-# }
+
+module "SG" {
+  source     = "../modules/SG"
+  depends_on = [module.vpc] # this depends_on is optional here since vpc_id = module.vpc.vpc_id already creates an implicit dependency.
+  vpc_id     = module.vpc.vpc_id
+  tags       = var.tags
+}
