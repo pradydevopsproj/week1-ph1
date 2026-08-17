9c15ce70051f7d76eff91a3d2716f66afd84bf86
pradydevopsproj <prady26devops@gmail.com>
2026-08-17 10:29:22 +0000

    checkov soft_failu true

--- Infra-repo/root/c4-outputs.tf
+++ Infra-repo/root/c4-outputs.tf
@@ -29,7 +29,7 @@ output "app_sg_id" {
   description = "The ID of the application tier EC2 vm SG"
 }
 
-# Output for ssh SG
+# Output  for ssh SG
 output "ssh_sg_id" {
   value       = module.SG.ssh_sg_id
   description = "The ID of the allow SSH SG"
